d7d503f3d1c44fae42ef37a37f2b6737d863ca20
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-03-28 21:36:53 +0900

    HelloWorld→空き家を捕まえましょう！に変更

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(writer http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(writer, "Hello World, %s!", request.URL.Path[1:])
+	fmt.Fprintf(writer, "空き家を捕まえましょう! %s", request.URL.Path[1:])
 }
 
 func main() {
