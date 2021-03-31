e423e8c329e29a4e28e3f3163e2df6384dd6287f
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-03-31 16:36:34 +0900

    index2.htmlを追加しpostページの生成

--- main.go
+++ main.go
@@ -10,14 +10,21 @@ import (
 
 func main() {
     // テンプレートのディレクトリを設定
-    goingtpl.SetBaseDir("./templates")
+    goingtpl.SetBaseDir("./app/views/templates")
 
     http.HandleFunc("/", handleTest)
+    http.HandleFunc("/post", handleTest2)
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handleTest(w http.ResponseWriter, r *http.Request) {
     // parent.htmlをパース
     tpl := template.Must(goingtpl.ParseFile("index.html"))
+    tpl.Execute(w, nil)
+}
+
+func handleTest2(w http.ResponseWriter, r *http.Request) {
+    // parent.htmlをパース
+    tpl := template.Must(goingtpl.ParseFile("index2.html"))
     tpl.Execute(w, nil)
 }
