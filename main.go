f74849f632518c56ede3f635d297b8d54dd9eb21
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-04-02 11:36:19 +0900

    config.go, config.iniを追加

--- main.go
+++ main.go
@@ -13,18 +13,20 @@ func main() {
     goingtpl.SetBaseDir("./app/views/templates")
 
     http.HandleFunc("/", handleTest)
-    http.HandleFunc("/post", handleTest2)
+    // http.HandleFunc("/post", handleTest2)
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func handleTest(w http.ResponseWriter, r *http.Request) {
     // parent.htmlをパース
-    tpl := template.Must(goingtpl.ParseFile("index.html"))
+    tpl := template.Must(goingtpl.ParseFile("layout.html"))
     tpl.Execute(w, nil)
 }
 
+/*
 func handleTest2(w http.ResponseWriter, r *http.Request) {
     // parent.htmlをパース
     tpl := template.Must(goingtpl.ParseFile("index2.html"))
     tpl.Execute(w, nil)
 }
+*/
