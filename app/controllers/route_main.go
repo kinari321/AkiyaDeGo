9b5845b5a48d854783ba94e7301f398ef9dd27f6
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-04-03 18:01:36 +0900

    post.htmlもレイアウトのテンプレート化（これで全て）

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -1,9 +1,6 @@
 package controllers
 
 import (
-	"github.com/playree/goingtpl"
-	"html/template"
-	"log"
 	"net/http"
 )
 
@@ -15,33 +12,6 @@ func handleMain(w http.ResponseWriter, r *http.Request) {
 	generateHTML(w, nil, "layout", "index")
 }
 
-var err error
-
-/*
-func handleTop(w http.ResponseWriter, r *http.Request) {
-	goingtpl.SetBaseDir("./app/views/templates")
-	tpl := template.Must(goingtpl.ParseFile("top.html"))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	tpl.Execute(w, nil)
-}
-*/
-/*
-func handleMain(w http.ResponseWriter, r *http.Request) {
-	goingtpl.SetBaseDir("./app/views/templates")
-	tpl := template.Must(goingtpl.ParseFile("index.html"))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	tpl.Execute(w, nil)
-}
-*/
 func handlePost(w http.ResponseWriter, r *http.Request) {
-	goingtpl.SetBaseDir("./app/views/templates")
-	tpl := template.Must(goingtpl.ParseFile("post.html"))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	tpl.Execute(w, nil)
+	generateHTML(w, nil, "layout", "post")
 }
