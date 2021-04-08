604a8e1930df52c5bdad7a7e5d86200e1ea84e8b
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-04-08 18:29:46 +0900

    handleLoginを追記

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -16,3 +16,6 @@ func handlePost(w http.ResponseWriter, r *http.Request) {
 func handleSignup(w http.ResponseWriter, r *http.Request) {
 	generateHTML(w, nil, "layout", "public_navbar", "signup")
 }
+func handleLogin(w http.ResponseWriter, r *http.Request) {
+	generateHTML(w, nil, "layout", "public_navbar", "login")
+}
