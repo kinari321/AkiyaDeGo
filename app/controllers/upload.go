09fc71d05889c631435ad8946d62b5dd87d22f24
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-04-22 17:46:35 +0900

    画像の保存場所を変更

--- app/controllers/upload.go
+++ app/controllers/upload.go
@@ -35,7 +35,7 @@ func handleUpload(w http.ResponseWriter, r *http.Request) {
 		}
 		defer file.Close()
 
-		f, err := os.Create("/tmp/test.jpg")
+		f, err := os.Create("./test.jpg")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -43,12 +43,12 @@ func handleUpload(w http.ResponseWriter, r *http.Request) {
 		defer f.Close()
 
 		io.Copy(f, file)
-		http.Redirect(w, r, "/show", http.StatusFound)
+		http.Redirect(w, r, "/show", 302)
 	}
 }
 
 func handleShow(w http.ResponseWriter, r *http.Request) {
-	file, err := os.Open("/tmp/test.jpg")
+	file, err := os.Open("./test.jpg")
 	defer file.Close()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -70,7 +70,7 @@ func generateHTMLWithImage(w http.ResponseWriter, data interface{}, m *image.Ima
 	for _, file := range filenames {
 		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
 	}
-	templates := template.Must(template.ParseFiles(files...))
+	// templates := template.Must(template.ParseFiles(files...))
 
 	buffer := new(bytes.Buffer)
 	if err := jpeg.Encode(buffer, *m, nil); err != nil {
@@ -80,6 +80,6 @@ func generateHTMLWithImage(w http.ResponseWriter, data interface{}, m *image.Ima
 	image := map[string]interface{}{"Image": str}
 	// renderTemplate(w, tmpl, data)
 	// generateHTML(w, data, "layout", "public_navbar", "show")
-	// templates := template.Must(template.ParseFiles(files...))
+	templates := template.Must(template.ParseFiles(files...))
 	templates.ExecuteTemplate(w, "layout", image)
 }
