ca3cdbcc1e1a4c0d50a2a132afc42fd9c74c1c22
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-04-28 20:49:47 +0900

    mysqlでdatabase作ることに成功

---
+++ app/models/base.go
@@ -0,0 +1,37 @@
+package models
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var (
+	Db  *sql.DB
+	err error
+)
+
+const (
+	tableNameUser = "users"
+)
+
+func init() {
+	Db, err = sql.Open("mysql", "akiya:password@(127.0.0.1:3306)/akiyadego?parseTime=true")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if err := Db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	cmdU := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s(
+		id INT NOT NULL AUTO_INCREMENT,
+		name TEXT NULL,
+		email TEXT NOT NULL,
+		password TEXT NOT NULL,
+		created_at DATETIME NULL,
+		PRIMARY KEY (id));`, tableNameUser)
+
+	Db.Exec(cmdU)
+}
