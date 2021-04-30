7c08ef1c603a6651e5ce7d22106f7f8da521aa47
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-04-30 09:05:12 +0900

    postでユーザー登録が出来る様に追記

--- app/controllers/route_auth.go
+++ app/controllers/route_auth.go
@@ -1,12 +1,33 @@
 package controllers
 
 import (
+	"AkiyaDeGo/app/models"
+	"fmt"
+	"log"
 	"net/http"
 )
 
 func handleSignup(w http.ResponseWriter, r *http.Request) {
-	generateHTML(w, nil, "layout", "public_navbar", "signup")
+	if r.Method == "GET" {
+		generateHTML(w, nil, "layout", "public_navbar", "signup")
+	} else if r.Method == "POST" {
+		err := r.ParseForm()
+		if err != nil {
+			log.Println(err)
+		}
+		user := models.User{
+			Name:     r.PostFormValue("name"),
+			Email:    r.PostFormValue("email"),
+			PassWord: r.PostFormValue("password"),
+		}
+		if err := user.CreateUser(); err != nil {
+			log.Println(err)
+		}
+		fmt.Println(user)
+		http.Redirect(w, r, "/top/", 302)
+	}
 }
+
 func handleLogin(w http.ResponseWriter, r *http.Request) {
 	generateHTML(w, nil, "layout", "public_navbar", "login")
 }
