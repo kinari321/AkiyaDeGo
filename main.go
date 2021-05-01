e2716ebc3cde3d55a165b3a9546cc93beacd3445
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-05-01 13:10:28 +0900

    Getpostsメモ

--- main.go
+++ main.go
@@ -3,29 +3,42 @@ package main
 import (
 	// "AkiyaDeGo/app/controllers"
 	"AkiyaDeGo/app/models"
-	"AkiyaDeGo/config"
+	// "AkiyaDeGo/config"
 	"fmt"
 	// "log"
 )
 
 func main() {
-	fmt.Println(config.Config.Port)
-	fmt.Println(models.Db)
+	// fmt.Println(config.Config.Port)
+	// fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test2"
-	u.Email = "test2@example.com"
-	u.PassWord = "testtest"
-	fmt.Println(u)
-	u.CreateUser()
+	// u := &models.User{}
+	// u.Name = "test2"
+	// u.Email = "test2@example.com"
+	// u.PassWord = "testtest"
+	// fmt.Println(u)
+	// u.CreateUser()
 
+	// user, _ := models.GetUser(2)
+	// p := &models.Post{}
+	// p.Title = "First Title"
+	// p.Description = "First Description"
+	// p.UserID = user.ID
+	// p.CreatePost()
+	// p, _ := models.GetPost(1)
+	// fmt.Println(p)
+	// ーーーーーーーーーーGetPostsーーーーーーーーーー
 	user, _ := models.GetUser(2)
 	p := &models.Post{}
-	p.Title = "First Title"
-	p.Description = "First Description"
+	p.Title = "Second Title"
+	p.Description = "Second Description"
 	p.UserID = user.ID
 	p.CreatePost()
-
+	posts, _ := models.GetPosts()
+	for _, v := range posts {
+		fmt.Println(v)
+	}
+	// ーーーーーーーーーーGetPostsーーーーーーーーーー
 	// u, _ := models.GetUser(1)
 	// fmt.Println(u)
 	// u.Name = "test2"
