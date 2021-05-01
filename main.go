11645a14fca38930297e5300393c8832c38e8b4b
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-05-01 10:42:18 +0900

    CreatePost

--- main.go
+++ main.go
@@ -1,53 +1,48 @@
 package main
 
 import (
-	"AkiyaDeGo/app/controllers"
+	// "AkiyaDeGo/app/controllers"
 	"AkiyaDeGo/app/models"
 	"AkiyaDeGo/config"
 	"fmt"
+	// "log"
 )
 
 func main() {
 	fmt.Println(config.Config.Port)
 	fmt.Println(models.Db)
 
-	// u := &models.User{}
-	// u.Name = "test2"
-	// u.Email = "test2@example.com"
-	// u.PassWord = "testtest"
-	// fmt.Println(u)
-	// u.CreateUser()
-	// u, _ := models.GetUser()
-	// fmt.Println(u)
+	u := &models.User{}
+	u.Name = "test2"
+	u.Email = "test2@example.com"
+	u.PassWord = "testtest"
+	fmt.Println(u)
+	u.CreateUser()
 
+	user, _ := models.GetUser(2)
+	p := &models.Post{}
+	p.Title = "First Title"
+	p.Description = "First Description"
+	p.UserID = user.ID
+	p.CreatePost()
+
+	// u, _ := models.GetUser(1)
+	// fmt.Println(u)
 	// u.Name = "test2"
 	// u.Email = "test2@example.com"
 	// u.UpdateUser()
 	// u, _ = models.GetUser(1)
 	// fmt.Println(u)
-
 	// u.DeleteUser()
-	// user, _ := models.GetUser(4)
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
+	// user, _ := models.GetUserByEmail("test@example.com")
+	// fmt.Println(user)
 
-	// user, _ := models.GetUser(1)
-	// p := &models.Post{}
-	// p.Title = "3 Post"
-	// p.Description = "3 Description"
-	// p.UserID = user.ID
-	// fmt.Println(p)
-	// p.CreatePost()
-
-	// posts, _ := models.GetPosts()
-	// for _, v := range posts {
-	// 	fmt.Println(v)
+	// session, err := user.CreateSession()
+	// if err != nil {
+	// 	log.Println(err)
 	// }
-
-	// p, _ := models.GetPost(3)
-	// p.Title = "Update Title"
-	// p.Description = "Update Description"
-	// p.UpdatePost()
-	// p, _ := models.GetPost(3)
-	// p.DeletePost()
-
-	controllers.StartMainServer()
+	// fmt.Println(session)
+	// controllers.StartMainServer()
 }
