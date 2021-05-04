5b4ff12e95d8c77863eb8e77f1d23c103112c2c5
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-05-04 11:49:49 +0900

    a.txtに書き込む状態のhandlePostを削除

--- app/controllers/route_main.go
+++ app/controllers/route_main.go
@@ -2,18 +2,9 @@ package controllers
 
 import (
 	"AkiyaDeGo/app/models"
-	// "fmt"
 	"log"
 	"net/http"
-	// "os"
 )
-
-// type Post struct {
-// 	Title      string
-// 	Category   string
-// 	Prefecture string
-// 	Opinion    string
-// }
 
 func handleTop(w http.ResponseWriter, r *http.Request) {
 	_, err := session(w, r)
@@ -27,47 +18,6 @@ func handleTop(w http.ResponseWriter, r *http.Request) {
 func handleMain(w http.ResponseWriter, r *http.Request) {
 	generateHTML(w, nil, "layout", "public_navbar", "index")
 }
-
-// func handlePost(w http.ResponseWriter, r *http.Request) {
-// 	generateHTML(w, nil, "layout", "public_navbar", "post")
-// 	if r.Method == "POST" {
-// 		post := Post{
-// 			Title:      r.PostFormValue("タイトル"),
-// 			Category:   r.PostFormValue("種類"),
-// 			Prefecture: r.PostFormValue("都道府県"),
-// 			Opinion:    r.PostFormValue("freeans"),
-// 		}
-// 		txt, err := os.OpenFile("a.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-// 		if err != nil {
-// 			log.Fatalln(err)
-// 		}
-// 		fmt.Fprintln(txt, "タイトル：	"+post.Title)
-// 		fmt.Fprintln(txt, "種類：　　　"+post.Category)
-// 		fmt.Fprintln(txt, "都道府県：	"+post.Prefecture)
-// 		fmt.Fprintln(txt, "body-start")
-// 		fmt.Fprintln(txt, post.Opinion)
-// 		fmt.Fprintln(txt, "body-end")
-// 		fmt.Fprintln(txt, "")
-// 	}
-// }
-
-// func handleIndex(w http.ResponseWriter, r *http.Request) {
-// 	_, err := session(w, r)
-// 	if err != nil {
-// 		http.Redirect(w, r, "/top/", 302)
-// 	} else {
-// 		generateHTML(w, nil, "layout", "private_navbar", "index")
-// 	}
-// }
-
-// func handleMytop(w http.ResponseWriter, r *http.Request) {
-// 	_, err := session(w, r)
-// 	if err != nil {
-// 		http.Redirect(w, r, "/top/", 302)
-// 	} else {
-// 		generateHTML(w, nil, "layout", "private_navbar", "mytop")
-// 	}
-// }
 
 func handleIndex(w http.ResponseWriter, r *http.Request) {
 	sess, err := session(w, r)
