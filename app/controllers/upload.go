d942597818b42c8657c1b12fce80c7ef7da9d735
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-05-18 09:16:23 +0900

    ローカルで画像一覧表示できるようにする

--- app/controllers/upload.go
+++ app/controllers/upload.go
@@ -26,13 +26,15 @@ func handleUpload(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-		file, _, err := r.FormFile("upload")
+		file, fileHeader, err := r.FormFile("upload")
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		defer file.Close()
-		f, err := os.Create("/usr/share/nginx/html/media/test.jpg")
+		uploadedFileName := fileHeader.Filename
+		// f, err := os.Create("/usr/share/nginx/html/media/" + uploadedFileName)	// EC2用
+		f, err := os.Create("/var/www/image/" + uploadedFileName) //ローカル用
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -45,35 +47,55 @@ func handleUpload(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleShow(w http.ResponseWriter, r *http.Request) {
-	file, err := os.Open("/usr/share/nginx/html/media/test.jpg")
-	defer file.Close()
+	// dir, err := os.Open("/usr/share/nginx/html/media/")	// EC2用
+	dir, err := os.Open("/var/www/image/") // ローカル用
+	defer dir.Close()
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	img, _, err := image.Decode(file)
-	m := resize.Resize(300, 0, img, resize.Lanczos3)
+	allImageNames, err := dir.Readdirnames(-1) // "/var/www/image/"のファイルの名前を配列に格納
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		log.Fatalln("No files")
 	}
-
-	generateHTMLWithImage(w, nil, &m, "layout", "public_navbar", "imageShow")
+	var decodeAllImages []image.Image // 配列decodeAllImagesを宣言
+	// 全ての画像をデコード、リサイズしてdecodeAllImageseに格納
+	for _, imageName := range allImageNames {
+		// file, _ := os.Open("/usr/share/nginx/html/media/" + imageName)	// EC2用
+		file, _ := os.Open("/var/www/image/" + imageName) //ローカル用
+		defer file.Close()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		decodeImage, _, err := image.Decode(file)
+		m := resize.Resize(300, 0, decodeImage, resize.Lanczos3)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		decodeAllImages = append(decodeAllImages, m)
+	}
+	generateHTMLWithImage(w, nil, decodeAllImages, "layout", "public_navbar", "imageShow")
 }
 
-func generateHTMLWithImage(w http.ResponseWriter, data interface{}, m *image.Image, filenames ...string) {
+func generateHTMLWithImage(w http.ResponseWriter, data interface{}, decodeAllImages []image.Image, filenames ...string) {
 	var files []string
 	for _, file := range filenames {
 		files = append(files, fmt.Sprintf("app/views/templates/%s.html", file))
 	}
 
-	buffer := new(bytes.Buffer)
-	if err := jpeg.Encode(buffer, *m, nil); err != nil {
-		log.Fatalln("Unable to encode image.")
+	var encordImages []string
+	for _, decodeImage := range decodeAllImages {
+		buffer := new(bytes.Buffer)
+		if err := jpeg.Encode(buffer, decodeImage, nil); err != nil {
+			log.Fatalln("Unable to encode image.")
+		}
+		str := base64.StdEncoding.EncodeToString(buffer.Bytes())
+		encordImages = append(encordImages, str)
 	}
-	str := base64.StdEncoding.EncodeToString(buffer.Bytes())
-	image := map[string]interface{}{"Image": str}
+
+	image := map[string]interface{}{"Images": encordImages}
 	templates := template.Must(template.ParseFiles(files...))
 	templates.ExecuteTemplate(w, "layout", image)
 }
