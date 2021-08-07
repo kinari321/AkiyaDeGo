d6a728c339ff78e8b6ac7b7a5934a5b4b920b8a5
K_Shirakawa <abcdream321@yahoo.co.jp>
2021-08-07 23:45:40 +0900

    configテストをスキップ

--- app/config/config_test.go
+++ app/config/config_test.go
@@ -1,14 +1,9 @@
 package config
 
 import (
-	"math"
 	"testing"
 )
 
-// 仮のテスト
-func TestAbs(t *testing.T) {
-	got := math.Abs(1)
-	if got != 1 {
-		t.Errorf("Abs(-1) = %f; want 1", got)
-	}
+func TestLoadConfig(t *testing.T) {
+	t.Skip("Skipping LoadConfig test")
 }
